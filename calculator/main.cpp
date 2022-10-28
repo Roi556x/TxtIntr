abd8cfe2e74df9f6b56c2cdc87e49e25893dcb62
Roi556x <112692127+Roi556x@users.noreply.github.com>
2022-10-28 10:59:55 +0300

    Add files via upload

---
+++ calculator/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <getopt.h>
+using namespace std;
+int main(int argc, char *argv[]) {
+setlocale(LC_ALL, "Rus");
+    { if (argc==1)
+        {
+            cout<<"„тобы вывести операцию суммы, напишите: -s (аргументы)"<< endl;
+            cout<<"„тобы вывести операцию вычитани€, напишите: -p (аргументы)"<<endl;
+        }
+    
+    char i, opt, summa = 0, substraction = 0, x, b=1, a;
+    while ((opt = getopt (argc, argv, "s:p")) != -1){
+        switch (opt) 
+        {
+            case 's':
+                for (i=2; i<argc; i++)
+                {
+                    x = strtol(argv[i], NULL, 10);
+                    summa=summa+x;
+                }
+                printf("Summa result is %d\n", summa);
+            break;
+            case 'p':
+            {
+                a= strtol(argv[2], NULL, 10);
+                for (int i=2; i<argc; i++)
+                {
+                    x = strtol(argv[i], NULL, 10);
+                   substraction=x-substraction;
+                    
+                }
+                printf("Substraction result is %d\n", substraction);
+            break;
+            }
+        }
+    }
+}
+}
